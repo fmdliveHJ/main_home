74d8ee5bcce8ea3395e9e1f75a9d55acf6b0f112
hyeonjin <gdalove@naver.com>
2023-10-22 21:59:28 +0900

    헤더 수정

--- src/components/layout/header.js
+++ src/components/layout/header.js
@@ -1,17 +1,20 @@
-import React from "react";
-import styles from "./header.module.css";
-import { Link } from "react-router-dom";
+import React from 'react';
+import styles from './header.module.css';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
     <div className={styles.header}>
+      <h1>
+        <Link to='/'>home</Link>
+      </h1>
       <nav>
         <ul>
           <li>
             <div>Front Experience</div>
             <ul>
               <li>
-                <Link to="/baronote">BaroNote</Link>
+                <Link to='/baronote'>BaroNote</Link>
               </li>
             </ul>
           </li>
@@ -19,13 +22,13 @@ const Header = () => {
             <div>Publishing Experience</div>
             <ul>
               <li>
-                <Link to="/Webjangi">웹쟁이</Link>
+                <Link to='/Webjangi'>웹쟁이</Link>
               </li>
               <li>
-                <Link to="/Hswf">화성시여성가족청소년재단</Link>
+                <Link to='/Hswf'>화성시여성가족청소년재단</Link>
               </li>
               <li>
-                <Link to="/Nowon">노원평생교육포털</Link>
+                <Link to='/Nowon'>노원평생교육포털</Link>
               </li>
             </ul>
           </li>
@@ -33,16 +36,16 @@ const Header = () => {
             <div>works Experience</div>
             <ul>
               <li>
-                <Link to="/baronote">Jeep</Link>
+                <Link to='/baronote'>Jeep</Link>
               </li>
               <li>
-                <Link to="/baronote">Nudie Jeans</Link>
+                <Link to='/baronote'>Nudie Jeans</Link>
               </li>
             </ul>
           </li>
         </ul>
       </nav>
-      <div className="theme"></div>
+      <div className='theme'></div>
     </div>
   );
 };
