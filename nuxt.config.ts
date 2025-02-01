203fe0a7301b796e5ddc86040b5a27c2bd33ffc3
hyeonjin <gdalove@naver.com>
2025-02-01 16:13:11 +0900

    feat: style, font 설정

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+// @ts-ignore
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+  css: [
+    '/assets/styles/main.scss'
+  ],
+
+  compatibilityDate: '2025-02-01',
 })
